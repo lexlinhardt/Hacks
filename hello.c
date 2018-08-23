d1d63dcc278c8ac0382c0b75dee43257f2e2724b
lexlinhardt <42552486+lexlinhardt@users.noreply.github.com>
2018-08-23 16:09:13 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Alexis Linhardt and Jack Keating
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Alexis Linhardt Computer Engineering\n");
+
+  return 0;
+}
