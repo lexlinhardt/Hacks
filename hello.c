c1cbb1e8aaaa65bbd55a221025c61e96c8ef35a5
jackkeating <42556832+jackkeating@users.noreply.github.com>
2018-08-23 16:37:03 -0500

    Changed my name

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Alexis Linhardt Computer Engineering, Jack Keating, Mechanical Engineering \n");
+  printf("Alexis Linhardt Computer Engineering, Jack Keating Mechanical Engineering \n");
 
   return 0;
 }
